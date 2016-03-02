c2037161152c3c7d932f2848019ae402cf335447
Daniel Walker <daniel@fattmerchant.com>
2016-03-01 22:05:56 -0500

    fix something

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 
 npm run build
 
-rm -rf build/svg
-rm -rf build/views
+rm -rf build/public/svg
+rm -rf build/public/views
 
-mkdir build/svg
-mkdir build/views
+mkdir -p build/public/svg
+mkdir -p build/public/views
 
-cp -r public/svg/* build/svg/
-cp -r public/views/* build/views/
+cp -r public/svg/* build/public/svg/
+cp -r public/views/* build/public/views/
 
 firebase deploy
