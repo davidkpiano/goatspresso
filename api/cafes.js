2714574ed87deb2d526ff9b4d1fb7665581cb29c
Daniel Walker <daniel@fattmerchant.com>
2016-03-01 19:37:40 -0500

    other commit

---
+++ api/cafes.js
@@ -0,0 +1,18 @@
+
+module.exports = [
+  {
+    name: 'Starbucks',
+    lat: "",
+    long: ""
+  },
+  {
+    name: 'Drunken Monkey',
+    lat: "",
+    long: ""
+  },
+  {
+    name: 'Downtown Credo',
+    lat: "",
+    long: ""
+  }
+];
